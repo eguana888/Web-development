59bc450024e0df4043b8dbdf8aadfcc4c6003806
unknown <workspace_@naer.com>
2023-12-04 21:56:44 +0900

    add timer,stopwatch,diary

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import Timer from './Timer';
+import Diary from './diary';
+import Stopwatch from './Stopwatch';
+
+export default function App() {
+  return <Diary />;
+}
