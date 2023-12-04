2a893271b7c8564063160d049fbfb413c3af3dfe
Styx <workspace_@naer.com>
2023-12-04 23:12:10 +0900

    map_final

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Calender from './Calender';
+import CurentMap from "./CurentMap";
 
 function App() {
   return (
-    <Calender/>
+    <CurentMap address={"경기도 용인시"}/>
   );
 }
 
